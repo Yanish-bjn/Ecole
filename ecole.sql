46f1983a4431dcdbacee28662b221df7b89ca7c0
Traasko <56584321+Traasko@users.noreply.github.com>
2020-10-27 08:36:21 +0100

    modification BDD

--- ecole.sql
+++ ecole.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1:3306
--- Généré le :  lun. 05 oct. 2020 à 15:17
+-- Généré le :  mar. 27 oct. 2020 à 07:35
 -- Version du serveur :  5.7.26
 -- Version de PHP :  7.2.18
 
@@ -43,7 +43,7 @@ CREATE TABLE IF NOT EXISTS `chat` (
 --
 
 INSERT INTO `chat` (`id`, `nom`, `prenom`, `date`, `message`) VALUES
-(1, 'Test2', 'Test2', ' 05 10 2020 05:17:12', 'Salut tout le monde !');
+(1, 'Yalap', 'Thomas', ' 05 10 2020 05:20:12', 'Bonjour');
 
 -- --------------------------------------------------------
 
@@ -57,22 +57,22 @@ CREATE TABLE IF NOT EXISTS `compte` (
   `nom` varchar(50) NOT NULL,
   `prenom` varchar(50) NOT NULL,
   `email` varchar(50) NOT NULL,
+  `ville` varchar(30) NOT NULL,
   `tel` varchar(20) NOT NULL,
   `mdp` varchar(50) NOT NULL,
   `role` varchar(10) NOT NULL,
-  PRIMARY KEY (`id`),
-  KEY `nom` (`nom`),
-  KEY `tel` (`tel`)
-) ENGINE=InnoDB AUTO_INCREMENT=17 DEFAULT CHARSET=utf8;
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=16 DEFAULT CHARSET=utf8;
 
 --
 -- Déchargement des données de la table `compte`
 --
 
-INSERT INTO `compte` (`id`, `nom`, `prenom`, `email`, `tel`, `mdp`, `role`) VALUES
-(3, 'bhujun', 'yanish', 'Yanish.bhujun@gmail.com', '0000000000', '164b6da6ab4209c16ff3841306dbec83', 'admin'),
-(9, 'Test', 'Test', 'Test@test.fr', '0000000000', '0cbc6611f5540bd0809a388dc95a615b', 'admin'),
-(16, 'Test2', 'Test2', 'Test2@test.fr', '0000000000', 'c454552d52d55d3ef56408742887362b', 'client');
+INSERT INTO `compte` (`id`, `nom`, `prenom`, `email`, `ville`, `tel`, `mdp`, `role`) VALUES
+(3, 'BHUJUN', 'YANISH', 'yanish.bhujun@gmail.com', '', '0782685300', '164b6da6ab4209c16ff3841306dbec83', 'admin'),
+(9, 'Yalap', 'Thomas', 'T.YALAP@lprs.fr', '', '0611111111', '3ed7dceaf266cafef032b9d5db224717', 'admin'),
+(14, 'ee', 'ee', 'ee@gmail.com', '', '0000000000', '08a4415e9d594ff960030b921d42b91e', 'client'),
+(15, 'cc', 'cc', 'cc@gmail.com', '', '0000000000', 'e0323a9039add2978bf5b49550572c7c', 'admin');
 
 -- --------------------------------------------------------
 
@@ -88,7 +88,7 @@ CREATE TABLE IF NOT EXISTS `contact` (
   `sujet` varchar(50) COLLATE utf8_bin NOT NULL,
   `message` varchar(200) COLLATE utf8_bin NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
 --
 -- Déchargement des données de la table `contact`
@@ -99,7 +99,8 @@ INSERT INTO `contact` (`id`, `nom`, `email`, `sujet`, `message`) VALUES
 (2, 'Yanish BHUJUN', 'yanish.bhujun@gmail.com', 'cdffffffffffffff', 'aaaaaaaaaasezftrgtrzgere'),
 (3, 'Yanish BHUJUN', 'yanish.bhujun@gmail.com', 'aaaaaabbbv', 'buibdfkhvbqdufvqbufsuref'),
 (4, 'Yanish BHUJUN', 'yanish.bhujun@gmail.com', ' dfvrfdgtrfdginutrbtibuit', ' cerfbvuiqebrrefrerfef'),
-(5, 'Yanish BHUJUN', 'yanish.bhujun@gmail.com', 'cdffffffffffffff', 'cdiucbeicdbcfiurfebehuer');
+(5, 'Yanish BHUJUN', 'yanish.bhujun@gmail.com', 'cdffffffffffffff', 'cdiucbeicdbcfiurfebehuer'),
+(6, 'Yalap', 'T.YALAP@lprs.fr', 'verbe', 'salutvytftyctycdtyuctycytty');
 
 -- --------------------------------------------------------
 
