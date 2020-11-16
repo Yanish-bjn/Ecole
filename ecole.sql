e73805de28ded55ac219fc1eb366c23e4dac7165
Traasko <56584321+Traasko@users.noreply.github.com>
2020-11-16 14:04:56 +0100

    Mise a jour BDD

--- ecole.sql
+++ ecole.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1:3306
--- Généré le :  lun. 09 nov. 2020 à 12:55
+-- Généré le :  lun. 16 nov. 2020 à 13:03
 -- Version du serveur :  5.7.26
 -- Version de PHP :  7.2.18
 
@@ -25,19 +25,129 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Structure de la table `chat`
+--
+
+DROP TABLE IF EXISTS `chat`;
+CREATE TABLE IF NOT EXISTS `chat` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nom` varchar(30) COLLATE utf8_bin NOT NULL,
+  `prenom` varchar(30) COLLATE utf8_bin NOT NULL,
+  `date` varchar(20) COLLATE utf8_bin NOT NULL,
+  `message` text COLLATE utf8_bin NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+--
+-- Déchargement des données de la table `chat`
+--
+
+INSERT INTO `chat` (`id`, `nom`, `prenom`, `date`, `message`) VALUES
+(6, 'Yalap', 'Thomas', ' 02-11-2020 17:06:03', 'Saluttttt');
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `compte`
+--
+
+DROP TABLE IF EXISTS `compte`;
+CREATE TABLE IF NOT EXISTS `compte` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nom` varchar(50) NOT NULL,
+  `prenom` varchar(50) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `ville` varchar(30) NOT NULL,
+  `tel` varchar(20) NOT NULL,
+  `mdp` varchar(50) NOT NULL,
+  `role` varchar(10) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=26 DEFAULT CHARSET=utf8;
+
+--
+-- Déchargement des données de la table `compte`
+--
+
+INSERT INTO `compte` (`id`, `nom`, `prenom`, `email`, `ville`, `tel`, `mdp`, `role`) VALUES
+(22, 'Yalap', 'Thomas', 'T.YALAP@lprs.fr', 'Groslay', '0641301961', '3ed7dceaf266cafef032b9d5db224717', 'admin'),
+(24, 'Bhujun', 'Yanish', 'yanish.bhujun@gmail.com', 'Saint-Denis', '0611111111', 'b8754b669cf0379bbee0011827a42e87', 'admin');
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `contact`
+--
+
+DROP TABLE IF EXISTS `contact`;
+CREATE TABLE IF NOT EXISTS `contact` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nom` varchar(50) COLLATE utf8_bin NOT NULL,
+  `email` varchar(50) COLLATE utf8_bin NOT NULL,
+  `sujet` varchar(50) COLLATE utf8_bin NOT NULL,
+  `message` varchar(200) COLLATE utf8_bin NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+--
+-- Déchargement des données de la table `contact`
+--
+
+INSERT INTO `contact` (`id`, `nom`, `email`, `sujet`, `message`) VALUES
+(1, 'Test', 'yanish.bhujun@gmail.com', 'aaaa', 'aaaaaaaaaaaavuhftyjdutyj'),
+(2, 'Yanish BHUJUN', 'yanish.bhujun@gmail.com', 'cdffffffffffffff', 'aaaaaaaaaasezftrgtrzgere'),
+(3, 'Yanish BHUJUN', 'yanish.bhujun@gmail.com', 'aaaaaabbbv', 'buibdfkhvbqdufvqbufsuref'),
+(4, 'Yanish BHUJUN', 'yanish.bhujun@gmail.com', ' dfvrfdgtrfdginutrbtibuit', ' cerfbvuiqebrrefrerfef'),
+(5, 'Yanish BHUJUN', 'yanish.bhujun@gmail.com', 'cdffffffffffffff', 'cdiucbeicdbcfiurfebehuer'),
+(6, 'Yalap', 'T.YALAP@lprs.fr', 'verbe', 'salutvytftyctycdtyuctycytty');
+
+-- --------------------------------------------------------
+
+--
 -- Structure de la table `emploi`
 --
 
 DROP TABLE IF EXISTS `emploi`;
 CREATE TABLE IF NOT EXISTS `emploi` (
-  `id` int(11) NOT NULL,
-  `nom_entreprise` varchar(50) COLLATE utf8_bin NOT NULL,
-  `adresse` varchar(50) COLLATE utf8_bin NOT NULL,
-  `rue` varchar(50) COLLATE utf8_bin NOT NULL,
-  `email` varchar(50) COLLATE utf8_bin NOT NULL,
-  `date` varchar(50) COLLATE utf8_bin NOT NULL,
-  `piece` varchar(100) COLLATE utf8_bin NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nom_entreprise` varchar(100) COLLATE utf8_bin NOT NULL,
+  `adresse` varchar(100) COLLATE utf8_bin NOT NULL,
+  `rue` varchar(100) COLLATE utf8_bin NOT NULL,
+  `email` varchar(100) COLLATE utf8_bin NOT NULL,
+  `date` varchar(100) COLLATE utf8_bin NOT NULL,
+  `piece` varchar(100) COLLATE utf8_bin NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+--
+-- Déchargement des données de la table `emploi`
+--
+
+INSERT INTO `emploi` (`id`, `nom_entreprise`, `adresse`, `rue`, `email`, `date`, `piece`) VALUES
+(1, 'Apple', 'ST DENIS', 'Rue des fleurs', 'T.YALAP@lprs.fr', '2020-11-06', '../fiche/BTS inscription.pdf');
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `evenement`
+--
+
+DROP TABLE IF EXISTS `evenement`;
+CREATE TABLE IF NOT EXISTS `evenement` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nom_evenement` varchar(50) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
+  `nom_personne` varchar(50) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
+  `date` date NOT NULL,
+  `description` varchar(50) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=latin1;
+
+--
+-- Déchargement des données de la table `evenement`
+--
+
+INSERT INTO `evenement` (`id`, `nom_evenement`, `nom_personne`, `date`, `description`) VALUES
+(8, 'JPO', 'Fizaine', '2020-11-02', 'Tournoi de Tennis de Table'),
+(10, 'Tournois', 'Noguez', '2020-11-28', 'Tournoi de Tennis de Table');
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
